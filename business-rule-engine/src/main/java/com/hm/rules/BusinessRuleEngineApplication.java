32406314598b8cea6863d3e5abf96e14a272d6d8
Administrator <Administrator@WIN-AFLPQ6DK22V>
2021-06-20 01:30:46 -0700

    initialization2

--- business-rule-engine/src/main/java/com/hm/rules/BusinessRuleEngineApplication.java
+++ business-rule-engine/src/main/java/com/hm/rules/BusinessRuleEngineApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BusinessRuleEngineApplication {
-  
+   
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessRuleEngineApplication.class, args);
 	}
