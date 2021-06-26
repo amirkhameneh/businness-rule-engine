57cabfcdd633c1c98b113830bafa138f5e32aec7
Administrator <Administrator@WIN-AFLPQ6DK22V>
2021-06-26 03:21:47 -0700

    Add Model Objects

--- business-rule-engine/src/main/java/com/hm/rules/model/entity/Rule.java
+++ business-rule-engine/src/main/java/com/hm/rules/model/entity/Rule.java
@@ -1,5 +1,57 @@
 package com.hm.rules.model.entity;
 
-public class Rule {
+import java.util.List;
 
+public class Rule {
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMatchingMethod() {
+		return matchingMethod;
+	}
+
+	public void setMatchingMethod(String matchingMethod) {
+		this.matchingMethod = matchingMethod;
+	}
+
+	public Zone getZone() {
+		return zone;
+	}
+
+	public void setZone(Zone zone) {
+		this.zone = zone;
+	}
+
+	public List<RuleRow> getRuleRows() {
+		return ruleRows;
+	}
+
+	public void setRuleRows(List<RuleRow> ruleRows) {
+		this.ruleRows = ruleRows;
+	}
+
+	private String id;
+	
+	private String name;
+	
+	private String matchingMethod;
+	
+	private Zone zone;
+	
+	private List<RuleRow> ruleRows;
+	
 }
--- business-rule-engine/src/main/java/com/hm/rules/model/entity/RuleRow.java
+++ business-rule-engine/src/main/java/com/hm/rules/model/entity/RuleRow.java
@@ -1,5 +1,89 @@
 package com.hm.rules.model.entity;
 
-public class RuleRow {
+import java.util.Date;
+import java.util.List;
 
+import com.hm.rules.model.dom.Operation;
+
+public class RuleRow {
+	
+	private String id;
+
+	private Rule rule;
+	
+	private Date fromDate;
+	
+	private Date toDate;
+	
+	private Integer priority;
+	
+	private String conditionalExpressionString;
+	
+	private List<Operation> conditionExpression;
+	
+	private String ActionExpressionString;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Rule getRule() {
+		return rule;
+	}
+
+	public void setRule(Rule rule) {
+		this.rule = rule;
+	}
+
+	public Date getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public Date getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
+
+	public Integer getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	public String getConditionalExpressionString() {
+		return conditionalExpressionString;
+	}
+
+	public void setConditionalExpressionString(String conditionalExpressionString) {
+		this.conditionalExpressionString = conditionalExpressionString;
+	}
+
+	public List<Operation> getConditionExpression() {
+		return conditionExpression;
+	}
+
+	public void setConditionExpression(List<Operation> conditionExpression) {
+		this.conditionExpression = conditionExpression;
+	}
+
+	public String getActionExpressionString() {
+		return ActionExpressionString;
+	}
+
+	public void setActionExpressionString(String actionExpressionString) {
+		ActionExpressionString = actionExpressionString;
+	}
 }
